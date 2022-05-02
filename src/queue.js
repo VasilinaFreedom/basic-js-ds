d49c0a7a63b25cbea7135d8a140f9f750b892be9
VasilinaFreedom <vasilinamkrvch@gmail.com>
2022-05-02 16:38:29 +0300

    create: add solution queue

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,86 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor (value) {
+    this.value = value;
+    this.next = null;
+    
   }
 }
+class Queue {
+ 
+    constructor () {
+      this.head = null;
+      this.tail = null;
+      this.queue = {}
+      this.length = 0;
+    }
+
+  getUnderlyingList() {
+    return this.head
+  }
+
+  enqueue(value) {
+    const node = new Node(value) //создаем новый элемент (узел)
+    if(this.head){ //если эл. есть в очереди
+      this.tail.next=node; //то следующий за за последним (хвостом) будет равен узлу (новому элементу)
+      this.tail=node; //и да, теперь в нашем хвосте новый узел(новый эл)
+    } else{
+      this.head=node; //если нет элементов, но тогда голова (первый) будет узлом
+      this.tail=node; //и она же хвост
+    }
+      this.length++; //увеличиваем длину очереди
+  }
+  
+
+  dequeue() {
+    const first=this.head;
+    this.head=this.head.next;
+    this.length--;
+    return first.value; //нам нужно удалить 1 элемент, он лежит в хэдере, теперь первый в хэдере будет равен следующему, длину очереди уменьшаем и возвращаем наш тот первый изначальный элемент
+  }
+  }
+
 
 module.exports = {
   Queue
 };
+
+// class Node {
+//   constructor (value) {
+//     this.value = value;
+//     this.next = null;
+    
+//   }
+// }
+
+// class Queue {
+//   constructor () {
+//     this.head = null;
+//     this.tail = null;
+//     this.queue = {}
+//     this.length = 0;
+//   }
+
+//   getUnderlyingList() {
+//     return this.queue
+//   }
+//   enqueue(value) {
+//     const node = new Node(value) //создаем новый элемент (узел)
+//     if(this.head){ //если эл. есть в очереди
+//       this.tail.next=node; //то следующий за за последним (хвостом) будет равен узлу (новому элементу)
+//       this.tail=node; //и да, теперь в нашем хвосте новый узел(новый эл)
+//     } else{
+//       this.head=node; //если нет элементов, но тогда голова (первый) будет узлом
+//       this.tail=node; //и она же хвост
+//     }
+//       this.length++; //увеличиваем длину очереди
+//   }
+//   dequeue() {
+//     const first=this.head;
+//     this.head=this.head.next;
+//     this.length--;
+//     return first.value; //нам нужно удалить 1 элемент, он лежит в хэдере, теперь первый в хэдере будет равен следующему, длину очереди уменьшаем и возвращаем наш тот первый изначальный элемент
+//   }
+// }
