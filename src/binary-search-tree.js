d119ae0eeda513fa70834efd78017828baf94f9c
VasilinaFreedom <vasilinamkrvch@gmail.com>
2022-05-03 01:48:59 +0300

    create: add tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,105 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
+  constructor() {
+    this.rootTree = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    return this.rootTree}
+  
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  add(data) {
+    this.rootTree = addElem(this.rootTree, data);
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+      function addElem(node, data) {
+        if (!node) {return new Node(data);}
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        if (node.data === data) {return node;}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+        if (data < node.data) {node.left = addElem(node.left, data);
+        } else {node.right = addElem(node.right, data);}
+
+        return node;
+      }
+    }
+    has(data) {
+      return searchElem(this.rootTree, data);
+
+      function searchElem(node, data) {
+        if (!node) {return false;}
+
+        if (node.data === data) {return true;}
+
+        return data < node.data ? searchElem(node.left, data) : searchElem(node.right, data);
+      }
+    }
+  
+  find(data) {
+    let node = this.data;
+    while(node!=null){
+      if(node.data===data){return node.data;}
+     else if(data>node.data){
+      node=node.right;
+    } else{
+      node=node.left;
+    }
+  } return null;
+}
+
+  remove(data) {
+    this.rootTree = removeElem(this.rootTree, data);
+
+    function removeElem(node, data) {
+      if (!node) {return null;}
+
+      if (data < node.data) {
+        node.left = removeElem(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeElem(node.right, data);
+        return node;
+      } else {
+          if (!node.left && !node.right) {return null;}
+
+        if (!node.left) {node = node.right;
+          return node;}
+
+        if (!node.right) {node = node.left;
+          return node;}
+
+        let rightMin = node.right;
+        while (rightMin.left) {rightMin = rightMin.left;}
+        node.data = rightMin.data;
+
+        node.right = removeElem(node.right, rightMin.data);
+
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootTree) {return null;}
+    let node = this.rootTree;
+    while (node.left) {node = node.left;}
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootTree) {return null;} //то же самое, только для правого
+    let node = this.rootTree;
+    while (node.right) {node = node.right;}
+    return node.data;
   }
 }
 
